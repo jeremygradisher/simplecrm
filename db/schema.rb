4a1a0e0cb16dfacd622573531d6e5a3de93952cc
Jeremy Gradisher <jeremygradisher@gmail.com>
2016-07-11 19:18:33 +0000

    Added grand_total column to Projects

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160711185424) do
+ActiveRecord::Schema.define(version: 20160711191019) do
 
   create_table "project_cicons", force: :cascade do |t|
     t.integer  "project_id"
@@ -67,6 +67,7 @@ ActiveRecord::Schema.define(version: 20160711185424) do
     t.datetime "created_at",        null: false
     t.datetime "updated_at",        null: false
     t.string   "phases"
+    t.string   "grand_total"
   end
 
   create_table "users", force: :cascade do |t|
